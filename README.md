52d4af80ee91ce2d5f7d0983943e803c2ed6068d
jeremiesigrist <37546982+jeremiesigrist@users.noreply.github.com>
2018-06-09 19:15:06 +0200

    sqd

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tensorflow
+index.html
--- sketch.js
+++ sketch.js
@@ -15,6 +15,8 @@ const hidden = tf.layers.dense({
   units: 4, // number of nodes
   inputShape: [2], // input shape
   activation: 'sigmoid'
+  inputShape: [1], // input shape
+  activation: 'linear'
 });
 // Add the layer
 model.add(hidden);
@@ -24,16 +26,26 @@ const output = tf.layers.dense({
   units: 1,
   // here the input shape is "inferred from the previous layer"
   activation: 'sigmoid'
+//  activation: 'sigmoid'
+  activation: 'linear'
 });
 model.add(output);
 
 // An optimizer using gradient descent
 const sgdOpt = tf.train.sgd(0.1);
+//const Opt = tf.train.sgd(0.1);
+const Opt = tf.train.sgd(0.1);
 
 // I'm done configuring the model so compile it
 model.compile({
   optimizer: sgdOpt,
+
+  optimizer: Opt,
   loss: tf.losses.meanSquaredError
+	//  loss: tf.losses.absoluteDifference
+	//  loss: tf.losses.hingeLoss
+	//  loss: tf.losses.logLoss
+
 });
 
 
@@ -42,12 +54,35 @@ const xs = tf.tensor2d([
   [0.5, 0.5],
   [1, 1],
   [0.2, 0.2]
+  [1, 0],
+  [0, 1],
+  [1, 1]
+]);
+
+const ys = tf.tensor2d([
+  [0],
+  [1],
+  [0.5],
+  [1],
+  [0]
+]);
+
+const zs = tf.tensor2d([
+  [0, 0],
+  [1, 0],
+  [0, 1],
+  [1, 1]
+=======
+  [0],
+//  [0.5],
+//  [1],
+  [0.2]
 ]);
 
 const ys = tf.tensor2d([
   [1],
-  [0.5],
-  [0],
+//  [0.5],
+//  [0],
   [0.8]
 ]);
 
@@ -58,14 +93,24 @@ const zs = tf.tensor2d([
   [0.99, 0.99],
   [1.99, 1.99],
   [0.0, 0.0]
+  [0.2],
+  [0.4],
+  [0.7],
+  [0.99],
+  [1.99],
+  [0.0]
+>>>>>>> c8e330ae9874c23b9deb1944871793395d324d74
 ]);
 
 var loadedModel;
+//var loadedModel;
 
 
 train().then(() => {
 
   let outputs = loadedModel.predict(zs);
+//  let outputs = loadedModel.predict(zs);
+  let outputs = model.predict(zs);
   outputs.print();
   console.log('training complete');
 });
@@ -76,16 +121,21 @@ async function train() {
   const loadedModel = await tf.loadModel('localstorage://my-model-1');
 
   console.log('Prediction from loaded model:');
+  console.log(loadedModel);
 
   for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 50; i++) {
     const config = {
       shuffle: true,
       epochs: 30
+      epochs: 100
     }
     const response = await loadedModel.fit(xs, ys, config);
+    const response = await model.fit(xs, ys, config);
     console.log(response.history.loss[0]);
   }
   const saveResults = await loadedModel.save('localstorage://my-model-1');
+  const saveResults = await model.save('localstorage://my-model-1');
 
 }
 
